a867c7c149815fc0ff3405a75d8893e8108d7c9b
Luke Van In <lukevanin@gmail.com>
2025-10-14 09:53:07 +0200

    Update documentation.

--- make-docs.sh
+++ make-docs.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+set -e
+
 # See https://swiftlang.github.io/swift-docc-plugin/documentation/swiftdoccplugin/publishing-to-github-pages
 
-swift package --allow-writing-to-directory docs/ \
+swift package --allow-writing-to-directory ./docs \
     generate-documentation --target SwiftQL \
     --disable-indexing \
     --transform-for-static-hosting \
-    --hosting-base-path /swiftql \
-    --output-path docs/
+    --hosting-base-path swiftql \
+    --output-path ./docs
+    
+touch docs/.nojekyll
+
+git add --all .
+
+git commit -m "Update documentation."
