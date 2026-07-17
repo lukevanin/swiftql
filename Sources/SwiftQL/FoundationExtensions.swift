97c35fd11dc70eb7772440987fd218521a229c61
Luke Van In <lukevanin@gmail.com>
2026-07-17 07:36:09 +0200

    Remove unused unsafe UUID conversion helpers
    Remove UUID.init(data:) and UUID.data() after confirming there are no first-party callers. This eliminates the unchecked short-buffer access without adding a replacement API.
    
    Refs #148

--- Sources/SwiftQL/FoundationExtensions.swift
+++ Sources/SwiftQL/FoundationExtensions.swift
@@ -20,26 +20,3 @@ extension Data {
         .joined()
     }
 }
-
-
-extension UUID {
-    
-    ///
-    /// Initializes a UUID with a Data object.
-    ///
-    init(data: Data) {
-        self = data.withUnsafeBytes { (p: UnsafeRawBufferPointer) -> UUID in
-            let b = p.bindMemory(to: uuid_t.self)
-            return UUID(uuid: b[0])
-        }
-    }
-
-    ///
-    /// Creates a Data object from the UUID.
-    ///
-    func data() -> Data {
-        withUnsafePointer(to: self) {
-            Data(bytes: $0, count: MemoryLayout.size(ofValue: self))
-        }
-    }
-}
